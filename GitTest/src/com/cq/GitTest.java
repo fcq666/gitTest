299283dbf91d50fabb400d3c8816f62c538b793c
cq <@qq.com>
2019-09-15 11:41:31 +0800

    第一次提交

---
+++ GitTest/src/com/cq/GitTest.java
@@ -0,0 +1,13 @@
+package com.cq;
+
+/**
+ * 作者:传奇
+ * 包名:com.cq
+ * 创建时间:2019/9/15 11:36
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("传奇6666666666");
+    }
+}
